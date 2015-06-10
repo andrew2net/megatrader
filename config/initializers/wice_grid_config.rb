7431caabf794cdd021f64fa08971475ad5c95058
andrew2net <android.2net@gmail.com>
2015-06-10 15:49:19 +0600

    1

--- config/initializers/wice_grid_config.rb
+++ config/initializers/wice_grid_config.rb
@@ -19,7 +19,8 @@ if defined?(Wice::Defaults)
   Wice::Defaults::CUSTOM_FILTER_ALL_LABEL = '--'
 
   # A list of classes for the table tag of the grid
-  Wice::Defaults::DEFAULT_TABLE_CLASSES = ['table', 'table-bordered', 'table-striped']
+  # Wice::Defaults::DEFAULT_TABLE_CLASSES = ['table', 'table-bordered', 'table-striped']
+  Wice::Defaults::DEFAULT_TABLE_CLASSES = ['table', 'table-striped']
 
   # Allow switching between a single and multiple selection modes in custom filters (dropdown boxes)
   Wice::Defaults::ALLOW_MULTIPLE_SELECTION = true
--- db/migrate/20150522075027_create_roles.rb
+++ db/migrate/20150522075027_create_roles.rb
@@ -5,11 +5,11 @@ class CreateRoles < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    Role.create(name: 'admins')
+    Role.create(name: 'admin')
     Role.create(name: 'editor')
 
     create_table :admins_roles do |t|
-      t.belongs_to :admins, index: true
+      t.belongs_to :admin, index: true
       t.belongs_to :role, index: true
     end
     admin = Admin.find_by_email('admins@local.loc')
