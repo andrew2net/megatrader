ac35ebe2a59e09e39f96c5b8e87f3da239c156ba
andrew2net <android.2net@gmail.com>
2015-07-08 19:03:39 +0600

    mail question

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/deploy/megatrader'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/images/pages}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/images}
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
