d4eed5dfddf725db0a2604f5e93c5d28eefba8d8
andrew2net <android.2net@gmail.com>
2016-04-09 15:42:16 +0600

    sidekiq schedule

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -4,7 +4,7 @@ puts "Sidekiq.server? is #{Sidekiq.server?.inspect}"
 puts "defined?(Rails::Server) is #{defined?(Rails::Server).inspect}"
 puts "defined?(Unicorn) is #{defined?(Unicorn).inspect}"
 
-# if Rails.env == 'production' && (defined?(Rails::Server) || defined?(Unicorn))
+if Rails.env == 'production' && (defined?(Rails::Server) || defined?(Unicorn))
   Sidekiq.configure_server do |config|
 
     config.on(:startup) do
@@ -13,19 +13,7 @@ puts "defined?(Unicorn) is #{defined?(Unicorn).inspect}"
       Sidekiq::Scheduler.reload_schedule!
     end
   end
-# else
-  # Sidekiq::Scheduler.enabled = false
-  # puts "Sidekiq::Scheduler.enabled is #{Sidekiq::Scheduler.enabled.inspect}"
-# end
-# Sidekiq.configure_server do |config|
-    # config.periodic do |mgr|
-    #       # see any crontab reference for the first argument
-    #       # e.g. http://www.adminschoice.com/crontab-quick-reference
-    #   mgr.register('* * * * *', Admin::GetCorrelationWorker)
-    #   #         mgr.register('0 * * * *', SomeHourlyWorkerClass)
-    #   #             mgr.register('* * * * *', SomeWorkerClass, retry: 2, queue: 'foo')
-    #   #                 mgr.register(cron_expression, worker_class, job_options={})
-    #   #                   end
-    #   #                   end
-    # end
-# end
+else
+  Sidekiq::Scheduler.enabled = false
+  puts "Sidekiq::Scheduler.enabled is #{Sidekiq::Scheduler.enabled.inspect}"
+end
