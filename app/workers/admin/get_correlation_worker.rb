2c347311ebbaffe8aa0e57c1aa9f59c686d434d7
andrew2net <android.2net@gmail.com>
2016-04-30 16:15:33 +0600

    correlation error

--- app/workers/admin/get_correlation_worker.rb
+++ app/workers/admin/get_correlation_worker.rb
@@ -8,12 +8,25 @@ class Admin::GetCorrelationWorker
         {Symbols: tools, TimeFrame: t.name, ReqType: 2}
       correlations = JSON.parse(resp.body)['Correlation']
       correlations.each do |c|
-        next if c[0] >= c[1]
+        next if c[0] == c[1]
         row_symbol = ToolSymbol.find_by name: c[0]
+        unless row_symbol
+          p "#{c[0]} not found"
+          next
+        end
         col_symbol = ToolSymbol.find_by name: c[1]
-        correlation = Correlation.find_or_create_by time_frame_id: t.id,
-          row_tool_symbol_id: row_symbol.id,
-          col_tool_symbol_id: col_symbol.id
+        unless col_symbol
+          p "#{c[1]} not found"
+          next
+        end
+        correlation = Correlation.find_by time_frame_id: t.id,
+          row_tool_symbol_id: col_symbol.id,
+          col_tool_symbol_id: row_symbol.id
+        unless correlation
+          correlation = Correlation.find_or_create_by time_frame_id: t.id,
+            row_tool_symbol_id: row_symbol.id,
+            col_tool_symbol_id: col_symbol.id
+        end
         correlation.update value: c[2]
       end
     end
