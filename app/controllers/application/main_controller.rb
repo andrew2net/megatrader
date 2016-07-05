783e2689a12ce147dd68c2f3ff1acbc6bd97a3e8
andrew2net <android.2net@gmail.com>
2016-07-05 21:27:16 +0600

    article doubled issue

--- app/controllers/application/main_controller.rb
+++ app/controllers/application/main_controller.rb
@@ -100,7 +100,8 @@ class Application::MainController < ApplicationController
 
   def articles
     logger.info "Locale: #{I18n.locale.to_s} #{I18n.locale.class}"
-    @articles = Page.articles.page(params[:page]).per(3)
+    @articles = Page.articles.with_translations(I18n.locale)
+      .page(params[:page]).per(3)
     @locale_sw = locale_sw MenuItem::URLS[:articles]
     respond_to do |format|
       format.html
