684c587615213ef48967596ed5d486ddc780113a
andrew2net <android.2net@gmail.com>
2017-02-15 19:24:24 +0100

    locale error

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  load_and_authorize_resource param_method: :admin_params
+  layout 'admin'
+  I18n.locale = :ru
+  add_breadcrumb '', :admin_root_path, options: {class: 'glyphicon glyphicon-home'}
+  add_breadcrumb I18n.t :users
+end
