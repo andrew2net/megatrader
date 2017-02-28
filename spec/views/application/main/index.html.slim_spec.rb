7388fc94bc725a3172025b9cd60fa9920552ebcb
andrew2net <android.2net@gmail.com>
2017-02-28 13:23:34 +0100

    video resposible on small screens

--- spec/views/application/main/index.html.slim_spec.rb
+++ spec/views/application/main/index.html.slim_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "application/main/index.html.erb", type: :view do
+RSpec.describe "application/main/index.html.slim", type: :view do
   context 'when display the pages' do
     it 'display the home page' do
       page = build(:home_page)
       assign :page, page
       assign :text, page.text
       render
-      expect(rendered).to match /Home page text/
+      expect(rendered).to match(/Home page text/)
     end
   end
 end
